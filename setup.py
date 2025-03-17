af065f43d125dbe271b9168ae494bf739ea88ccc
yhwu <wuyuheng20074@live.com>
2025-03-17 20:26:03 +0800

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name="rl_asset_allocation",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pytest"
+    ],
+    python_requires=">=3.7"
+)
